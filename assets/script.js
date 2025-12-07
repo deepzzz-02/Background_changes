e37af172e159d5097dbdcc82a7b1aaf7b6295433
Deepa <ndeepaaa02@gmail.com>
2025-12-07 09:39:23 +0530

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,48 @@
+
+
+function blue(){
+   document.body.style.backgroundImage= "url('/background_change/bg_images/blue_bg.jpg')";
+   document.body.style.backgroundRepeat="no-repeat";
+      document.body.style.backgroundSize="cover";
+      let quotes =document.getElementById('quotes')
+      quotes.innerHTML="Blue is the color of truth and transparency.!";
+      
+}
+function pink(){
+   document.body.style.backgroundImage= "url('/background_change/bg_images/pink_bg.jpg')";
+   document.body.style.backgroundRepeat="no-repeat";
+      document.body.style.backgroundSize="cover";
+      let quotes =document.getElementById('quotes')
+quotes.innerHTML="Blushing pink is the color of my heart when I think of you.!" ;
+}
+function yellow(){
+   document.body.style.backgroundImage= "url('/background_change/bg_images/yellow_bg.jpg')";
+   document.body.style.backgroundRepeat="no-repeat";
+      document.body.style.backgroundSize="cover";
+      let quotes =document.getElementById('quotes')
+quotes.innerHTML="Sunshine is not just a light; it’s a state of mind.!" ;
+}
+function red() {
+   document.body.style.backgroundImage="url('/background_change/bg_images/red_bg.jpg')";
+   document.body.style.backgroundRepeat="no-repeat";
+   document.body.style.backgroundSize="cover";
+let quotes=document.getElementById('quotes')
+quotes.innerHTML="Red is the first color of spring. It’s the real color of rebirth, of beginning!";
+   
+   
+}
+function black(){
+   document.body.style.backgroundImage= "url('/background_change/bg_images/black_bg.jpg')";
+   document.body.style.backgroundRepeat="no-repeat";
+      document.body.style.backgroundSize="cover";
+      let quotes =document.getElementById('quotes')
+      quotes.innerHTML="Find your power, even in the darkest shade.!";
+}
+function purple(){
+   document.body.style.backgroundImage= "url('/background_change/bg_images/purple_bg.jpg')";
+   document.body.style.backgroundRepeat="no-repeat";
+      document.body.style.backgroundSize="cover";
+let quotes = document.getElementById("quotes");
+quotes.innerText = "purple does something strange to me!";
+
+}
